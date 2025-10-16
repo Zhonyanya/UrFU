80cb96b459223717cc303e8770053ba963df1e8c
Zhonyanya <andreevich10050@gmail.com>
2025-10-16 10:04:29 +0400

    Create task3.py

---
+++ programming/practice4/task3.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+class Book:
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+
+
+    def info(self) -> str:
+        return f"Книга {self.title} написана автором {self.author} и выпущена в {self.year} году"
+    
+
+    @classmethod
+    def from_string(cls, data):
+        title, author, year = data.split(";")
+        return cls(title, author, int(year))
+    
+
+    @property
+    def age(self) -> int:
+        return datetime.now().year - self.year
+    
+    @age.setter
+    def age(self, value):
+        self.year = datetime.now().year - value
+
+
+    def __str__(self) -> str:
+        return self.info()
+    
+
+    def __eq__(self, other) -> bool:
+        return isinstance(other, Book) and self.title == other.title
