b89a791ce403e9acb482974e19de693f4898bd76
Zhonyanya <andreevich10050@gmail.com>
2025-10-25 10:10:16 +0400

    Update task4.py

--- programming/practice4/task4.py
+++ programming/practice4/task4.py
@@ -7,20 +7,43 @@ class Printable(ABC):
 
 class Book(Printable):
     def __init__(self, title, author, year):
+        """
+        Класс книги
+
+        Args:
+            title (str): Название
+            author (str): Автор
+            year (int): Год выхода
+        """
         self.title = title
         self.author = author
         self.year = year
 
 
     def info(self) -> str:
+        """
+        Возвращает инфу
+        """
         return f"Книга {self.title} написана автором {self.author} и выпущена в {self.year} году"
     
     def print_info(self):
+        """
+        Выводит инфу
+        """
         print(self.info())
     
 
     @classmethod
     def from_string(cls, data):
+        """
+        Генерирует объект класса Book через строку
+        
+        Args:
+            cls: Название класса
+            data (str): Строка через которую создаётся класс
+        Returns:
+            cls (Book): Объект класса Book
+        """
         title, author, year = data.split(";")
         return cls(title, author, int(year))
     
@@ -35,8 +58,14 @@ class Book(Printable):
 
 
     def __str__(self) -> str:
+        """
+        Позволяет принтовать нормально
+        """
         return self.info()
     
 
     def __eq__(self, other) -> bool:
+        """
+        Позволяет сравнивать две книги
+        """
         return isinstance(other, Book) and self.title == other.title
