8236a0d4427fda43d8a88b5572100aa0d3193eb8
Zhonyanya <andreevich10050@gmail.com>
2025-10-25 10:08:21 +0400

    Update task1.py

--- programming/practice4/task1.py
+++ programming/practice4/task1.py
@@ -1,7 +1,18 @@
 class Book:
     def __init__(self, title, author, year):
+        """
+        Класс книги
+
+        Args:
+            title (str): Название
+            author (str): Автор
+            year (int): Год выхода
+        """
         self.title = title
         self.author = author
         self.year = year
     def info(self) -> str:
+        """
+        Возвращает инфу
+        """
         return f"Книга {self.title} написана автором {self.author} и выпущена в {self.year} году"
