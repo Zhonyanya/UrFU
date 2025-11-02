1ce8ef482e61b284b54796ca9adb280f9f7d14a0
Zhonyanya <andreevich10050@gmail.com>
2025-11-02 13:49:39 +0400

    Update pokemon_info.py

--- programming/practice6/pokemon_info.py
+++ programming/practice6/pokemon_info.py
@@ -2,11 +2,15 @@ import requests
 response = requests.get("https://pokeapi.co/api/v2/pokemon")
 responselist = response.json()["results"]
 pokedict = dict()
+pokenamelist = []
 for pokemon in responselist:
     pokedict[pokemon['name']] = pokemon['url']
+    pokenamelist.append(pokemon["name"])
+print(pokenamelist)
+
 def get_pokemon_stats(pokemon_name: str):
     """
-    Выдаёт данные покемона: имя, типы, вес, рост, названия способностей
+    Выдаёт данные покемона: имя, тип, вес, рост, названия способностей
 
     Args:
         pokemon_name (str): Имя покемона на английском
@@ -27,4 +31,10 @@ def get_pokemon_stats(pokemon_name: str):
         abilist.append(abi["ability"]["name"])
     return {"name": pokemon_name, "types": types, "weight": weight,
              "height": height, "abilities": abilist}
-# print(get_pokemon_stats("beedrill")["abilities"])
+pokename = input("Имя покемона: ")
+pokestats = get_pokemon_stats(pokename)
+print(f"Имя: {pokestats['name']}")
+print(f"Типы: {pokestats['types']}")
+print(f"Вес: {pokestats['weight']}")
+print(f"Рост: {pokestats['height']}")
+print(f"Способности: {pokestats['abilities']}")
